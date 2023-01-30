864aa8c23869db053b2ac1c000026cb8c1fd0e6f
teacher <vincentjava@yahoo.com.tw>
2023-01-30 15:56:44 +0800

    python

---
+++ day01/Hello4.py
@@ -0,0 +1,12 @@
+import math
+# 變數的應用
+# 半徑 r = 9
+# 求圓面積與球體積 (算到小數點1位)
+r = 9
+pi = math.pi
+print(pi)
+area = pi * r**2
+print('圓面積 = %.1f' % area)
+volume = 4/3 * pi * r**3
+print('球體積 = %.1f' % volume)
+
