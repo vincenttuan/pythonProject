307e8a5fe6f83e0158106e13336ee2d00fc7de77
teacher <vincentjava@yahoo.com.tw>
2023-01-31 16:32:04 +0800

    python

--- day02/Test10.py
+++ day02/Test10.py
@@ -1,2 +1,8 @@
 words = '小明本薪$65000,今年公司發放6個月,試問小明年終'
-
+print(words[5:10])
+print(words[words.find('$')+1:words.find(',')])
+print(words[words.find('發放')+2:words.find('個月')])
+# --------------------------------------------------------
+salary = int(words[words.find('$')+1:words.find(',')])
+month = int(words[words.find('發放')+2:words.find('個月')])
+print(salary * month)
