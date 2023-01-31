7dd82cd34a3062c77ada6ab7716800fb29e5ced9
teacher <vincentjava@yahoo.com.tw>
2023-01-31 14:24:39 +0800

    python

---
+++ day02/Test4.py
@@ -0,0 +1,13 @@
+# 隨機數
+import random as r
+
+num = r.randint(1, 5)  # 含 1 與 5 的隨機數 1 <= r <= 5
+print(num)
+num = r.randrange(1, 5)  # 含 1 不含 5 的隨機數 1 <= r < 5
+print(num)
+
+# 樂透電腦選號三星彩 (0~9) 選 3 個可以重複的數字
+n1 = r.randint(0, 9)
+n2 = r.randint(0, 9)
+n3 = r.randint(0, 9)
+print('電腦選號: %d %d %d' % (n1, n2, n3))
