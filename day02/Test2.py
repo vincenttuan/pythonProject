9db94da703cf94dca9bd8ba550aaef0a75cab670
teacher <vincentjava@yahoo.com.tw>
2023-01-31 13:57:08 +0800

    python

---
+++ day02/Test2.py
@@ -0,0 +1,11 @@
+# 資料轉型 (變態 = 改變資料型態)
+# str -> int
+# str -> float
+# int -> str etc...
+chinese = '100'
+english = 80
+math = '70.5'
+chinese = int(chinese)
+math = float(math)
+total = chinese + english + math
+print(total)
