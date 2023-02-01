21f315ceeda5a54a6aab24f02e64d0b545ebd743
teacher <vincentjava@yahoo.com.tw>
2023-02-01 15:44:11 +0800

    python

--- day03/猜數字.py
+++ day03/猜數字.py
@@ -5,6 +5,7 @@ min, max = 0, 100
 answer = r.randint(min + 1, max - 1)
 
 while True:
+    # 玩家:
     guess = input('玩家 {} ~ {} 之間猜一個數字: '.format(min, max))
     guess = int(guess)
     if guess < answer:
@@ -14,5 +15,13 @@ while True:
     else:
         print('答案: {} 恭喜玩家答對了'.format(answer))
         break
-
-
+    # 電腦
+    pc_guess = r.randint(min + 1, max - 1)
+    print('電腦 {} ~ {} 之間猜一個數字: {}'.format(min, max, pc_guess))
+    if pc_guess < answer:
+        min = pc_guess
+    elif guess > answer:
+        max = pc_guess
+    else:
+        print('答案: {} 恭喜電腦答對了'.format(answer))
+        break
