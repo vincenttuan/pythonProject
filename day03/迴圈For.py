95c874b88d8fae0340052c061e8cb3fd2d066bb9
teacher <vincentjava@yahoo.com.tw>
2023-02-01 16:07:58 +0800

    python

--- day03/迴圈For.py
+++ day03/迴圈For.py
@@ -10,3 +10,10 @@ for i in range(0, 10):  # 0~9, 預設i每次+1
 # ---------------------
 for i in range(0, 10, 2):  # 0~9, i每次+2
     print(i, 'Python')
+# ---------------------
+for score in [100, 90, 80, 70, 60]:
+    print(score)
+# ---------------------
+for i, score in enumerate([100, 90, 80, 70, 60]):
+    print(i, score)
+
