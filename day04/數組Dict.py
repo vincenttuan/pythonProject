10471836f0ae76431ee5f86089d2cd41162bff26
teacher <vincentjava@yahoo.com.tw>
2023-02-02 15:55:49 +0800

    python

---
+++ day04/數組Dict.py
@@ -0,0 +1,7 @@
+# 字典數組 : Dict
+emp1 = {'name': 'John', 'salary': 5000}
+print(emp1)
+print(emp1['name'], type(emp1['name']))
+print(emp1['salary'], type(emp1['salary']))
+emp1['salary'] = 55000
+print(emp1['salary'])
