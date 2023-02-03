c09cccf707592b0b69141f0c3c9b9bc662d06125
teacher <vincentjava@yahoo.com.tw>
2023-02-03 14:35:44 +0800

    python

--- day05/使用自訂涵式.py
+++ day05/使用自訂涵式.py
@@ -1,6 +1,6 @@
 from day05.自訂涵式1 import hello
 from day05.自訂涵式2 import add
-from day05.自訂涵式3 import calcBmi
+from day05.自訂涵式3 import calcBmi, get_bmi, verify_bmi
 
 # 主程式
 if __name__ == '__main__':
@@ -9,4 +9,6 @@ if __name__ == '__main__':
      calcBmi(170, 50)
      calcBmi(180, 70)
      calcBmi(160, 60)
-
+     bmi = get_bmi(170, 60)
+     print(bmi)
+     print(verify_bmi(30.5))
