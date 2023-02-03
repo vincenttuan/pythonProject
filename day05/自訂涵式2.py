d9cec18b0e0233598daf084779b5772ded050cb5
teacher <vincentjava@yahoo.com.tw>
2023-02-03 14:05:20 +0800

    python

--- day05/自訂涵式2.py
+++ day05/自訂涵式2.py
@@ -1,1 +1,15 @@
 # 自訂涵式
+
+def add(x, y):  # 無回傳值方法
+    print(x + y)
+
+
+def get_add(x, y):  # 有回傳值方法
+    return x + y
+
+
+# 主程式
+if __name__ == '__main__':
+    add(10, 20)
+    result = get_add(10, 20)
+    print(result)
