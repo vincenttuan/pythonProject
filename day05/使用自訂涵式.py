59f2fb8481260a63be1bd5e7952a8346994abaa8
teacher <vincentjava@yahoo.com.tw>
2023-02-03 14:49:13 +0800

    python

--- day05/使用自訂涵式.py
+++ day05/使用自訂涵式.py
@@ -1,14 +1,16 @@
 from day05.自訂涵式1 import hello
 from day05.自訂涵式2 import add
 from day05.自訂涵式3 import calcBmi, get_bmi, verify_bmi
-
+from day05.自訂涵式4 import add_and_sub
 # 主程式
 if __name__ == '__main__':
-     hello()
-     add(10, 20)
-     calcBmi(170, 50)
-     calcBmi(180, 70)
-     calcBmi(160, 60)
-     bmi = get_bmi(170, 60)
-     print(bmi)
-     print(verify_bmi(30.5))
+    hello()
+    add(10, 20)
+    calcBmi(170, 50)
+    calcBmi(180, 70)
+    calcBmi(160, 60)
+    bmi = get_bmi(170, 60)
+    print(bmi)
+    print(verify_bmi(30.5))
+    res1, res2 = add_and_sub(10, 5)
+    print(res1, res2)
---
+++ day05/自訂涵式4.py
@@ -0,0 +1,16 @@
+# 自訂涵式
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def add_and_sub(x, y):
+    result1 = add(x, y)
+    result2 = sub(x, y)
+    return result1, result2
+
+
+
