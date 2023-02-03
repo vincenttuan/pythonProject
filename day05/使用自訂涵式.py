5febbe80882a3a948961cd5b7bf0d2d0fe785c25
teacher <vincentjava@yahoo.com.tw>
2023-02-03 14:28:16 +0800

    python

--- day05/使用自訂涵式.py
+++ day05/使用自訂涵式.py
@@ -1,8 +1,12 @@
 from day05.自訂涵式1 import hello
 from day05.自訂涵式2 import add
+from day05.自訂涵式3 import calcBmi
 
 # 主程式
 if __name__ == '__main__':
      hello()
      add(10, 20)
+     calcBmi(170, 50)
+     calcBmi(180, 70)
+     calcBmi(160, 60)
 
--- day05/自訂涵式3.py
+++ day05/自訂涵式3.py
@@ -2,3 +2,15 @@
 # 請試做一個 def calcBmi 方法
 # calcBmi 方法可以輸入 2 個參數
 # 可以印出 bmi 值 與 bmi 是否正常 (18<bmi<=23 正常範圍)
+def calcBmi(h, w):
+    # 1. 計算 bmi 值
+    bmi = w / (h/100)**2
+    # 2. 驗證 bmi
+    result = '正常'  # 假設驗證結果正常
+    if bmi > 23:
+        result = '過胖'
+    elif bmi <= 18:
+        result = '過輕'
+    # 3. 印出資料
+    print('BMI: {0:.1f} {1}'.format(bmi, result))
+
