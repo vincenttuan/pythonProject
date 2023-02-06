e55ed3b9b7809d625b2382fc8ca1574434eb92d6
teacher <vincentjava@yahoo.com.tw>
2023-02-06 16:07:30 +0800

    python

--- day06/AnalysisFile2.py
+++ day06/AnalysisFile2.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import statistics as stat
 from day06.ReadFile import get_employees
 
 if __name__ == '__main__':
@@ -11,11 +12,17 @@ if __name__ == '__main__':
     print(names)
     print(salary)
     print(age)
+    # 計算 CV
+    salary_cv = stat.stdev(salary) / stat.mean(salary)
+    age_cv = stat.stdev(age) / stat.mean(age)
 
     plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
     plt.rcParams['axes.unicode_minus'] = False
-    plt.plot(names, salary, marker="o", label="薪資(K)")
-    plt.plot(names, age, marker="o", label="年齡")
+    plt.plot(names, salary, marker="o", label="薪資(K) CV:{0:.2f}% 平均:{1:,.1f}K".format(salary_cv*100, stat.mean(salary)))
+    plt.plot(names, age,    marker="o", label="年齡 CV:{0:.2f}% 平均:{1:.1f}".format(age_cv*100, stat.mean(age)))
     plt.legend()
     plt.grid(True)
+    plt.ylabel("年齡/薪資(K)")
+    plt.xlabel("姓名")
+    plt.title("員工資料統計表")
     plt.show()
