e6a83d2c4a89268ac7e17653015be8a9009ef2af
teacher <vincentjava@yahoo.com.tw>
2023-02-06 14:56:57 +0800

    python

--- day06/AnalysysFile1.py
+++ day06/AnalysysFile1.py
@@ -15,5 +15,16 @@ if __name__ == '__main__':
     print('薪資中位數: {:,}'.format(stat.median(salary)))
     print('薪資標準差 SD: {:,.1f}'.format(stat.stdev(salary)))
     print('薪資變異係數 CV: {:.2f}'.format(stat.stdev(salary)/stat.mean(salary)))
-
-
+    # --------------------------------------------------------------------------
+    # 資料讀取
+    employee_age = get_employees('age.txt', column2='age')
+    print(employee_age)
+    # 資料分析(年齡)
+    # 取得所有年齡[] -> [25, 52, 45, ...]
+    age = [emp['age'] for emp in employee_age]
+    print(age)
+    print('年齡總和: {:,}'.format(sum(age)))
+    print('年齡平均: {:,}'.format(stat.mean(age)))
+    print('年齡中位數: {:,}'.format(stat.median(age)))
+    print('年齡標準差 SD: {:,.1f}'.format(stat.stdev(age)))
+    print('年齡變異係數 CV: {:.2f}'.format(stat.stdev(age) / stat.mean(age)))
