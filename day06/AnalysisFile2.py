9fb561476d1f597d87c06463d6f6784cad99da84
teacher <vincentjava@yahoo.com.tw>
2023-02-06 15:54:52 +0800

    python

--- day06/AnalysisFile2.py
+++ day06/AnalysisFile2.py
@@ -1,5 +1,21 @@
+import matplotlib.pyplot as plt
 from day06.ReadFile import get_employees
 
 if __name__ == '__main__':
     employees = get_employees('salary_age.txt', column3='age')
     print(employees)
+    # 繪製薪資與年齡的折線圖
+    names = [emp['name'] for emp in employees]
+    salary = [emp['salary']//1000 for emp in employees]  # 將薪資除以 1000
+    age = [emp['age'] for emp in employees]
+    print(names)
+    print(salary)
+    print(age)
+
+    plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.plot(names, salary, marker="o", label="薪資(K)")
+    plt.plot(names, age, marker="o", label="年齡")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
