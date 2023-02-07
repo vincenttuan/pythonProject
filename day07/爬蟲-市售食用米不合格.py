7e239e98a8bd1bf0da17f4191e5c94cfbee9f89a
teacher <vincentjava@yahoo.com.tw>
2023-02-07 14:30:23 +0800

    python

--- day07/爬蟲-市售食用米不合格.py
+++ day07/爬蟲-市售食用米不合格.py
@@ -6,7 +6,26 @@ if __name__ == '__main__':
     # 1. 取得原始資料
     url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
     source = requests.get(url).text
-    print(type(source))
-    print(source)
+    # print(type(source))
+    # print(source)
+    # 2. 將 json str 轉為 數組 以利後續分析
+    data = json.loads(source)  # data 就是可分析的數組(list)
+    print('資料型態:', type(data))
+    print('資料筆數:', len(data))
+    print('資料內容:', data)
+    # 3. 決定要使用的資料欄位有哪些(option) ?
+    # 品名, 廠商名稱, 不合格原因, 行政處分
+    bad_rice = []
+    for item in data:
+        rice = {}
+        rice.setdefault('品名', item['品名'])
+        rice.setdefault('廠商名稱', item['廠商名稱'])
+        rice.setdefault('不合格原因', item['不合格原因'])
+        rice.setdefault('行政處分', item['行政處分'])
+        bad_rice.append(rice)
+    print('bad_rice:')
+    print(bad_rice)
+
+
 
 
