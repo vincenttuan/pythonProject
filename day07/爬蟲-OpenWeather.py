ea6ec562d83af0188923a447861ed4fd633c7b93
teacher <vincentjava@yahoo.com.tw>
2023-02-07 15:25:15 +0800

    python

--- day07/爬蟲-OpenWeather.py
+++ day07/爬蟲-OpenWeather.py
@@ -1,1 +1,7 @@
 # 網路位置: https://api.openweathermap.org/data/2.5/weather?q={},tw&appid={}
+if __name__ == '__main__':
+    city_name = 'taipei'
+    key = 'your key'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={},tw&appid={}'.format(city_name, key)
+    print(url)
+
