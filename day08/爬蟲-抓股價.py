bc3343656077f49a0eeb039fdbafb7d1649906a0
teacher <vincentjava@yahoo.com.tw>
2023-02-08 15:54:26 +0800

    python

--- day08/爬蟲-抓股價.py
+++ day08/爬蟲-抓股價.py
@@ -9,6 +9,10 @@ def get_stock_price(stonk_no):
     tag_name = 'span'
     class_value = 'Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)'
     tag = soup.find(name=tag_name, attrs={"class": class_value})
+    if tag is None:
+        class_value = 'Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)'
+        tag = soup.find(name=tag_name, attrs={"class": class_value})
+
     # print(tag)
     return tag.text
 
