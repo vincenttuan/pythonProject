6278fe217de855320835f484409506a7df20f278
teacher <vincentjava@yahoo.com.tw>
2023-02-08 16:15:44 +0800

    python

---
+++ day08/Line_Notify.py
@@ -0,0 +1,12 @@
+# 發送位置: https://notify-api.line.me/api/notify
+# token = ody4CpIUb1Su3Wof2sUdxaWrVF7zzqLc5XApBqg36Od
+import requests
+
+if __name__ == '__main__':
+    url = 'https://notify-api.line.me/api/notify'
+    token = 'ody4CpIUb1Su3Wof2sUdxaWrVF7zzqLc5XApBqg36Od'
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+
+
