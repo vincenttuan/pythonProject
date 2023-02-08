76eb293bd9ade298ab4be2f42357062e2c2d8689
teacher <vincentjava@yahoo.com.tw>
2023-02-08 14:51:26 +0800

    python

--- day08/爬蟲-台灣交易所.py
+++ day08/爬蟲-台灣交易所.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date={}&selectType=ALL'.format(date_str)
     data = requests.get(url).text
     # print(data)
+    stocks = []
     for row in data.split("\r\n"):
         stock = row.split('","')
         if len(stock) == 7:
@@ -25,7 +26,10 @@ if __name__ == '__main__':
                 dict.setdefault('本益比', float(stock[4]))
                 dict.setdefault('股價淨值比', float(stock[5]))
                 dict.setdefault('財報', stock[6].replace('",', ''))
-                print(dict)
+                # print(dict)
+                stocks.append(dict)
             except ValueError:
                 pass
 
+    print(len(stocks))
+    print(stocks)
