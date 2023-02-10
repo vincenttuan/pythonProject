b07bb2f8d4c791ab33f77b2f0603be45df5a7552
teacher <vincentjava@yahoo.com.tw>
2023-02-10 14:18:46 +0800

    python

--- day10/偵測照片.py
+++ day10/偵測照片.py
@@ -4,3 +4,18 @@ import cv2
 # 人臉特徵檔
 face_cascade = cv2.CascadeClassifier("./haarcascade/haarcascade_frontalface_default.xml")
 print(face_cascade)
+
+# 讀取影像(原始圖)
+frame = cv2.imread("./sample_image/test.jpg")
+
+# 讀取灰階影像
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# 顯示影像
+cv2.imshow('My image', gray)
+
+# 按下任意鍵離開
+c = cv2.waitKey(0)
+print(c)
+
+
